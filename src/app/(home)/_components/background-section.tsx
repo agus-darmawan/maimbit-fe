91de195d4d545b19f8b7c1106c28144b834e1865
agus-darmawan <wayanagus.dr@gmail.com>
2025-09-07 11:51:05 +0800

    feat: fix homepage bg  section

--- src/app/(home)/_components/background-section.tsx
+++ src/app/(home)/_components/background-section.tsx
@@ -2,14 +2,24 @@ import Image from "next/image";
 
 export default function BackgroundSection() {
   return (
-    <section className="py-16 bg-gradient-to-b from-gray-50 to-white text-justify font-medium">
+    <section className="py-16 bg-gradient-to-b from-gray-50 to-white font-medium">
       <div className="container mx-auto px-6">
         <div className="grid md:grid-cols-2 gap-12 items-start text-xl">
-          <div>
-            <h2 className="text-4xl md:text-5xl font-bold text-blue-900 mb-6">
+          <div className="flex justify-center items-end order-1 md:order-2">
+            <Image
+              src="/images/homepage/maps/kalteng.png"
+              alt="Peta Administrasi Kalimantan Tengah"
+              className="rounded-lg shadow-md ml-auto"
+              width={431}
+              height={431}
+            />
+          </div>
+
+          <div className="order-2 md:order-1">
+            <h2 className="text-4xl md:text-5xl font-bold text-blue-900 mb-6 text-center md:text-left">
               Latar Belakang Maimbit
             </h2>
-            <p className="text-gray-800 leading-relaxed pt-10">
+            <p className="text-gray-800 leading-relaxed pt-10 text-justify">
               Di dunia yang penuh ketidakpastian, Maimbit hadir untuk membantu
               generasi muda menemukan jalan mereka. Kami adalah solusi terpadu
               yang membantu Anda mengenali potensi diri, mengakses peluang
@@ -19,19 +29,10 @@ export default function BackgroundSection() {
               strategis dan penuh makna.
             </p>
           </div>
-
-          <div className="flex justify-center items-end">
-            <Image
-              src="/images/homepage/maps/kalteng.png"
-              alt="Peta Administrasi Kalimantan Tengah"
-              className="rounded-lg shadow-md ml-auto"
-              width={431}
-              height={431}
-            />
-          </div>
         </div>
+
         <div className="mt-10">
-          <p className="text-gray-800 leading-relaxed text-xl">
+          <p className="text-gray-800 leading-relaxed text-xl text-justify">
             Masa depan membutuhkan tindakan. Maimbit mengajak Anda untuk tidak
             hanya bermimpi, tetapi juga beraksi. Kami menyediakan alat dan
             komunitas untuk mengubah potensi menjadi kenyataan dan kepedulian
